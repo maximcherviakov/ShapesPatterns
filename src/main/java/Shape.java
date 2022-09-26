b2a1ebe37017e54a41698c83702c804daf739090
maxim <cherviakovmaxim@gmail.com>
2022-09-26 20:00:25 +0300

    All classes were added

---
+++ src/main/java/Shape.java
@@ -0,0 +1,29 @@
+public abstract class Shape implements Cloneable {
+    private String id;
+    protected String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    protected Object clone() throws CloneNotSupportedException {
+        Object clone = null;
+
+        try {
+            clone = super.clone();
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return clone;
+    }
+}
---
+++ src/main/java/ShapeCache.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class ShapeCache {
+    private static HashMap<String, Shape> shapeMap = new HashMap<String, Shape>();
+
+    public static Shape getShape(String id) {
+        return shapeMap.get(id);
+    }
+
+    public static void loadCache() {
+        Circle circle = new Circle();
+        circle.setId("1");
+        shapeMap.put(circle.getId(),circle);
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.setId("2");
+        shapeMap.put(rectangle.getId(), rectangle);
+
+        Square square = new Square();
+        square.setId("3");
+        shapeMap.put(square.getId(),square);
+    }
+}
